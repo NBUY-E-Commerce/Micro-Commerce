6f41496a89f371ea4800b387fdd9281837dcd379
direncancatalkaya <direncan.catalkaya@gmail.com>
2019-11-14 23:40:07 +0300

    ReflectionDelete

--- B-Commerce.Common/Repository/Repository.cs
+++ B-Commerce.Common/Repository/Repository.cs
@@ -25,7 +25,46 @@ namespace B_Commerce.Common.Repository
 
         public void Delete(T entity)
         {
-            _context.Entry(entity).State = EntityState.Deleted;
+
+            //var ct = _context.ChangeTracker.Entries().ToList();
+
+            entity.isDeleted = true;
+
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+            madeIsDeleteTrueForChilds(entity);
+            void madeIsDeleteTrueForChilds(T entityy)
+            {
+                var type = typeof(T);
+                foreach (PropertyInfo VARIABLE in type.GetProperties())
+                {
+                    if (VARIABLE.PropertyType.Namespace == "System.Collections.Generic")
+                    {
+                        var repotype = typeof(Repository<>);
+                        Type[] typeOfChild = VARIABLE.PropertyType.GenericTypeArguments;
+                        var repo = repotype.MakeGenericType(typeOfChild);
+                        dynamic repoOfChild = Activator.CreateInstance(repo, _context);
+
+                        _context.SaveChanges();
+
+                        var listtype = typeof(List<>);
+                        var list = listtype.MakeGenericType(typeOfChild);
+                        dynamic listofprop = Activator.CreateInstance(list);
+
+                        //Burada ListofProp u instancei alınmıs nesnenin liste olan propertysine eşitleyip childlarında dönmem lazım ancak reflection 
+                        //instance in propertylerini vermiyor sorun orda ??
+
+                        foreach (var item in listofprop)
+                        {
+                            item.isDeleted = 1;
+                            repoOfChild.Update(item);
+                            _context.SaveChanges();
+                        }
+                    }
+                }
+            }
+
+            
         }
 
         public IQueryable<T> Get(Expression<Func<T, bool>> filter = null)
