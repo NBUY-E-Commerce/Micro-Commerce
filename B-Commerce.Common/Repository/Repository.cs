d386ac2be8d8e393463de4b9bb32f8a55cff14d6
direncancatalkaya <direncan.catalkaya@gmail.com>
2019-11-18 23:50:07 +0300

    NotificationService

--- B-Commerce.Common/Repository/Repository.cs
+++ B-Commerce.Common/Repository/Repository.cs
@@ -10,11 +10,12 @@ using System.Text;
 
 namespace B_Commerce.Common.Repository
 {
-    public class Repository<T> : IRepository<T> where T : BaseEntity,new()
+    public class Repository<T> : IRepository<T> where T : BaseEntity, new()
     {
         private DbContext _context;
         private DbSet<T> _dbSet;
-        public Repository(DbContext context) {
+        public Repository(DbContext context)
+        {
             _context = context;
             _dbSet = _context.Set<T>();
         }
@@ -25,52 +26,14 @@ namespace B_Commerce.Common.Repository
 
         public void Delete(T entity)
         {
-
-            //var ct = _context.ChangeTracker.Entries().ToList();
-
-            entity.isDeleted = true;
-
-            _context.Entry(entity).State = EntityState.Modified;
-            _context.SaveChanges();
-            madeIsDeleteTrueForChilds(entity);
-            void madeIsDeleteTrueForChilds(T entityy)
-            {
-                var type = typeof(T);
-                foreach (PropertyInfo VARIABLE in type.GetProperties())
-                {
-                    if (VARIABLE.PropertyType.Namespace == "System.Collections.Generic")
-                    {
-                        var repotype = typeof(Repository<>);
-                        Type[] typeOfChild = VARIABLE.PropertyType.GenericTypeArguments;
-                        var repo = repotype.MakeGenericType(typeOfChild);
-                        dynamic repoOfChild = Activator.CreateInstance(repo, _context);
-
-                        _context.SaveChanges();
-
-                        var listtype = typeof(List<>);
-                        var list = listtype.MakeGenericType(typeOfChild);
-                        dynamic listofprop = Activator.CreateInstance(list);
-
-                        //Burada ListofProp u instancei alınmıs nesnenin liste olan propertysine eşitleyip childlarında dönmem lazım ancak reflection 
-                        //instance in propertylerini vermiyor sorun orda ??
-
-                        foreach (var item in listofprop)
-                        {
-                            item.isDeleted = 1;
-                            repoOfChild.Update(item);
-                            _context.SaveChanges();
-                        }
-                    }
-                }
-            }
-
-            
+            _context.Entry(entity).State = EntityState.Deleted;
         }
 
         public IQueryable<T> Get(Expression<Func<T, bool>> filter = null)
         {
-            if (filter==null) { 
-                return _dbSet.AsQueryable(); 
+            if (filter == null)
+            {
+                return _dbSet.AsQueryable();
             }
             return _dbSet.Where(filter).AsQueryable();
         }
