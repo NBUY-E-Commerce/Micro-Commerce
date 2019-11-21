50114663f7ae905db0f0aab156411efabbeba3f2
ViktorReznov111 <onursen1990@hotmail.com>
2019-11-21 21:51:06 +0200

    api default1

---
+++ B-Commerce.LoginApi/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using B_Commerce.Common.Repository;
+using B_Commerce.Common.UOW;
+using B_Commerce.Login.Common;
+using B_Commerce.Login.DatabaseContext;
+using B_Commerce.Login.DomainClass;
+using B_Commerce.Login.Response;
+using B_Commerce.Login.Service.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B_Commerce.LoginApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        LoginDbContext loginDbContext;
+        IUnitOfWork _unitOfWork;
+        IRepository<User> _userRepository;
+        IRepository<AccountVerification> _accountVerificationRepository;
+        CacheManager CacheManager;
+
+        [HttpPost]
+        public LoginResponse Login(string email, string password)
+        {
+
+            LoginService loginService = new LoginService(_unitOfWork, _userRepository, _accountVerificationRepository, CacheManager);
+
+            LoginResponse loginResponse = new LoginResponse();
+
+            loginResponse = loginService.Login(new B_Commerce.Login.Request.LoginRequest { Email = email, Password = password });
+
+            return loginResponse;
+
+
+        }
+    }
+}
