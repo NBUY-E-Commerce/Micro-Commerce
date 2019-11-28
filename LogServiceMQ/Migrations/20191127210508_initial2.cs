a5975c155cae3701375dfaebc36e574ef2f1cc4d
Sezgin Çolak <sezgincolak90@gmail.com>
2019-11-28 21:20:12 +0300

    new

---
+++ LogServiceMQ/Migrations/20191127210508_initial2.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LogService.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LogInfos",
+                table: "LogInfos");
+
+            migrationBuilder.DropColumn(
+                name: "LogInfoID",
+                table: "LogInfos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LogID",
+                table: "LogInfos",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LogInfos",
+                table: "LogInfos",
+                column: "LogID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LogInfos",
+                table: "LogInfos");
+
+            migrationBuilder.DropColumn(
+                name: "LogID",
+                table: "LogInfos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogInfoID",
+                table: "LogInfos",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LogInfos",
+                table: "LogInfos",
+                column: "LogInfoID");
+        }
+    }
+}
---
+++ LogServiceMQ/MyDbContext/LogDbContext.cs
@@ -0,0 +1,42 @@
+﻿using LogService.DomainClasses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace LogService.MyDbContext
+{
+
+
+    internal class LogDbContext : DbContext
+    {
+        private string _connectionString;
+        public LogDbContext()
+        {
+
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+         .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+         .AddJsonFile("appsettings.json")
+         .Build();
+            _connectionString = configuration.GetConnectionString("LogServiceDB");
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        public virtual DbSet<LogInfo> LogInfos { get; set; }
+
+
+    }
+
+
+}
+
