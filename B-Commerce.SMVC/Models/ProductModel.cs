354d1e2084ea94d6d1c29904b36b118b2b4d6ff8
Ömer <faruk.benli@hotmail.com>
2019-12-02 22:15:55 +0300

    Product Sliderının ve gerekliliklerinin eklenmesi.

---
+++ B-Commerce.SMVC/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace B_Commerce.SMVC.Models
+{
+    public class ProductModel
+    {
+        public int? ID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public int AvailableCount { get; set; }
+        public bool isActive { get; set; } = true;
+        public int CategoryID { get; set; }
+        public bool IsInSlider { get; set; }
+        public bool IsInNew { get; set; }
+        public bool IsInHot { get; set; }
+        public bool IsInGift { get; set; }
+    }
+}
