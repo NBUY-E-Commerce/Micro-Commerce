b444864358e99078eff51165ca4f496449eab3f0
direncancatalkaya <direncan.catalkaya@gmail.com>
2019-12-06 00:02:17 +0300

    ProductDetay

--- B-Commerce.ProductService/Service/Concrete/ProductService.cs
+++ B-Commerce.ProductService/Service/Concrete/ProductService.cs
@@ -70,6 +70,10 @@ namespace B_Commerce.ProductService.Service.Concrete
                     ProductName = product.ProductName,
                     Size = product.Size
                 };
+                foreach (ProductImage item in product.ProductImages)
+                {
+                    response.GetProductModel.ImageUrls.Add(item.URL);
+                }
                 response.SetStatus(Constants.ResponseCode.SUCCESS);
                 return response;
             }
