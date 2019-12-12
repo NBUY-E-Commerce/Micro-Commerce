cbc86548685a66da34e79626408bd88fab991f93
onur sen <onursen1990@hotmail.com>
2019-12-12 12:47:37 +0300

    .

--- B-Commerce.ProductService/Service/Concrete/ProductService.cs
+++ B-Commerce.ProductService/Service/Concrete/ProductService.cs
@@ -185,7 +185,7 @@ namespace B_Commerce.ProductService.Service.Concrete
                 }
 
                 productResponse.PagingInfo = new PagingInfo(request.Page, request.Range, allProductCount);
-                productResponse.CategoryDescription = _repositoryProduct.Get(t => t.CategoryID == request.CategoryID).FirstOrDefault().Category.Description;
+                productResponse.CategoryDescription = products.FirstOrDefault().Category.CategoryName;
                 productResponse.SetStatus(Constants.ResponseCode.SUCCESS);
                 return productResponse;
             }
