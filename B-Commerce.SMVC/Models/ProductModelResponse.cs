dde848e5cb59cf7e3d6c3be0306bc46b2821e7f3
Sezgin Çolak <sezgin.colak@hotmail.com.tr>
2019-12-12 11:55:22 +0300

    product category todo done.

--- B-Commerce.SMVC/Models/ProductModelResponse.cs
+++ B-Commerce.SMVC/Models/ProductModelResponse.cs
@@ -8,10 +8,13 @@ namespace B_Commerce.SMVC.Models
 {
     public class ProductModelResponse : CommonResponse
     {
+
         public List<ProductModel> Products { get; set; } = new List<ProductModel>();
         public Dictionary<string, int> ProductsColor { get; set; } = new Dictionary<string, int>();
         public List<BrandFilterModel> ProductsBrand { get; set; } = new List<BrandFilterModel>();
         public PagingInfo PagingInfo { get; set; } = new PagingInfo();
+
+        public string CategoryDescription { get; set; }
 
     }
 }
